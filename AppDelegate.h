2103526326e572841d5a921b35058e2b46bde725
Mack Chamberlain <jeffjet24@gmail.com>
2014-09-24 14:37:29 -0500

    First File

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MOBA
+//
+//  Created by Mack on 9/19/14.
+//  Copyright (c) 2014 Mack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
