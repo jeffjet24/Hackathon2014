df588b1864c690fcdfedb7ebea47e37ffa2abbbf
Alisa1296 <alisa.corgard@gmail.com>
2014-09-24 14:45:58 -0500

    Playing around with Turtle

---
+++ IceCreamTurtle.py
@@ -0,0 +1,13 @@
+import turtle
+turtle.setup(800, 600)
+turtle.getturtle()
+turtle.goto(300,0)
+turtle.goto(0, -270)
+turtle.goto(-300, 0)
+turtle.home()
+turtle.penup()
+turtle.goto(0, -100)
+turtle.goto(0, -150)
+turtle.pendown()
+turtle.circle(250)
+ 
